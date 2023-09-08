828b67184eae6ab98ffdcf1b47df87ac118a2e29
zerosanityy <akosikenneth099@gmail.com>
2023-09-08 21:13:50 +0800

    added arraylist and declaration of Strings

--- src/main/java/pikei/App.java
+++ src/main/java/pikei/App.java
@@ -4,12 +4,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
-
+import java.util.ArrayList;
 import org.openqa.selenium.WebDriver;
 public class App 
 {
-    public static void main( String[] args )
+    public void getData ()
     {
+        System.out.println("Hello world");
+    }
+    public static void main( String[] args )
+    {   
+        App something = new App();
+        something.getData();
         //invoking the browser
         //Chrome -> Chrome Driver - > Methods
         /* 
@@ -26,7 +32,6 @@ public class App
         int[] arr = {1,2,3,4,5};
         String[] names = {"Paul", "Kenneth", "Ilan"};
         int sum = 0;
-
         //adding array elements
         for (int i=0; i < arr.length; i++)
         {
@@ -38,6 +43,28 @@ public class App
         {
             System.out.println(names[j]);
         }
-
+        //arraylist
+        ArrayList<String> a = new ArrayList<String>();
+        a.add("Ken");
+        a.add("Ilan");
+        a.add("Paul");
+        a.add("Automation testing");
+        System.out.println(a);
+        System.out.println(a.get(0));
+        for (int v=0; v<a.size(); v++)
+        {
+            System.out.println(a.get(v));
+        }
+        System.out.println(a.contains("Hatdog"));
+        
+        String myNameUlit = "Paul Kenneth B. Ilan";
+        String[] splittedName = myNameUlit.split(" ");//splitted into 4 values
+        for (int p=0; p<splittedName.length; p++)
+        {
+            System.out.println(splittedName[p]);
+        }
+        
+        //Creating methods
+        
     }
 }
