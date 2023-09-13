6cfe5aff63591a096475295e14df1a52fefc7184
zerosanityy <akosikenneth099@gmail.com>
2023-09-13 14:36:04 +0800

    Selenium Locators

--- src/main/java/pikei/SeleniumLocator.java
+++ src/main/java/pikei/SeleniumLocator.java
@@ -5,21 +5,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.ArrayList;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-public class App 
+public class SeleniumLocator 
 {
     public static void main( String[] args )
     {   
        
         //invoking the browser
         //Chrome -> Chrome Driver - > Methods
-        
+        String username ="PaulKenneth";
         System.setProperty("webdriver.chrome.driver","D:/development/Java Selenium Projects/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.facebook.com/");
-        System.out.println(driver.getTitle());
-        System.out.println(driver.getCurrentUrl());
-        driver.close();
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        System.out.println("Title is: " +driver.getTitle());
+        System.out.println("Current Url is: " + driver.getCurrentUrl());
+        driver.findElement(By.id("inputUsername")).sendKeys(username);
+        driver.findElement(By.name("inputPassword")).sendKeys("Passwordsample123");
         
         
         
