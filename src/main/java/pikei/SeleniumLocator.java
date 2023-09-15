b176014b72af503703cdb96b3916efe6b1ad77b2
zerosanityy <akosikenneth099@gmail.com>
2023-09-15 15:06:16 +0800

    added css selector and implcitwait

--- src/main/java/pikei/SeleniumLocator.java
+++ src/main/java/pikei/SeleniumLocator.java
@@ -3,6 +3,7 @@ package pikei;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.lang.reflect.Array;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.ArrayList;
 
@@ -18,12 +19,24 @@ public class SeleniumLocator
         String username ="PaulKenneth";
         System.setProperty("webdriver.chrome.driver","D:/development/Java Selenium Projects/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
         System.out.println("Title is: " +driver.getTitle());
         System.out.println("Current Url is: " + driver.getCurrentUrl());
         driver.findElement(By.id("inputUsername")).sendKeys(username);
         driver.findElement(By.name("inputPassword")).sendKeys("Passwordsample123");
+        driver.findElement(By.id("chkboxOne")).click();
+        driver.findElement(By.id("chkboxTwo")).click();
         driver.findElement(By.className("signInBtn")).click();
+        
+        //CSS SELECTOR input[placeholder="Username"] if the html code don't have an id
+        //Tagname.classname also works
+        
+        System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+
+        
+        
+        
         
         
     }
