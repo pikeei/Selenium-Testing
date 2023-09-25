c1914e3468ebffd4cc7310fab10578fa68396d1d
zerosanityy <akosikenneth099@gmail.com>
2023-09-25 20:39:35 +0800

    added static dropdowns automation

--- src/main/java/pikei/WindowActivities.java
+++ src/main/java/pikei/WindowActivities.java
@@ -1,14 +1,13 @@
 package pikei;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import java.lang.reflect.Array;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.ArrayList;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.By.ByXPath;
+import org.openqa.selenium.*;
 
 
 public class WindowActivities {
@@ -17,14 +16,27 @@ public class WindowActivities {
         String username ="PaulKenneth";
         String email = username + "@gmail.com";
         String phoneNumber = "09123456789";
-        System.setProperty("webdriver.chrome.driver","D:/development/Java Selenium Projects/chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver","D:/development/Java Selenium Projects/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
         driver.get("https://www.google.com");
         driver.navigate().to("https://rahulshettyacademy.com/AutomationPractice/");
         driver.navigate().back();
         driver.navigate().forward();
+        driver.navigate().to("https://rahulshettyacademy.com/dropdownsPractise/"); 
+        WebElement staticDropdown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+        Select dropdown = new Select(staticDropdown);
+        dropdown.selectByIndex(0);
+        dropdown.selectByValue("INR");
+        System.out.println(dropdown.getFirstSelectedOption().getText());
+        dropdown.selectByValue("AED");
+        System.out.println(dropdown.getFirstSelectedOption().getText());
+        dropdown.selectByVisibleText("USD");
+        System.out.println(dropdown.getFirstSelectedOption().getText());
+
+
     }       
 
 }
